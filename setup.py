9dc05fcae82f0f08ea895b63b74d0e19f4054791
BlackSamsung <siddharthshringi@gmail.com>
2018-08-10 18:25:13 +0530

    add classifiers

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
+import setuptools
 
 setup(
     name='gitpy',
-    version='0.1',
+    version='1.0',
+    url="https://github.com/SiddharthShringi/Gitpy",
+    autor="Siddharth Shringi",
+    autor_email="siddharthshrigi@gmail.com",
+    description="A Command Line Interface for accessing Github api.",
+    long_description=open('README.md').read(),
+    packages=setuptools.find_packages(),
     py_modules=['gitpy'],
     install_requires=[
         'Click', 'Requests',
@@ -11,4 +18,9 @@ setup(
         [console_scripts]
         gitpy=gitpy:cli
     ''',
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
 )
