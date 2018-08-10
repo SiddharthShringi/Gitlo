35325cb62ae96df9fd48671c8bbe69b3a3b5770d
BlackSamsung <siddharthshringi@gmail.com>
2018-08-10 19:52:39 +0530

    change command from gitpu to pygit

--- setup.py
+++ setup.py
@@ -2,25 +2,24 @@ from setuptools import setup
 import setuptools
 
 setup(
-    name='gitpy',
+    name='pygit',
     version='1.0',
     url="https://github.com/SiddharthShringi/Gitpy",
-    autor="Siddharth Shringi",
-    autor_email="siddharthshrigi@gmail.com",
+    author="Siddharth Shringi",
+    author_email="siddharthshrigi@gmail.com",
     description="A Command Line Interface for accessing Github api.",
     long_description=open('README.md').read(),
     packages=setuptools.find_packages(),
-    py_modules=['gitpy'],
+    py_modules=['pygit'],
     install_requires=[
         'Click', 'Requests',
     ],
     entry_points='''
         [console_scripts]
-        gitpy=gitpy:cli
+        pygit=pygit:cli
     ''',
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
     ),
 )
